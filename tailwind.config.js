37f65498dd00f70d5adf4de5d10904e0f378d9a9
Mohd-Salman-0119 <salmanansari910550@gmail.com>
2023-12-19 12:28:07 +0530

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "primary": "#0D263B",
+        "secondry": "#0061DF"
+      }
+    },
+  },
+  plugins: [],
+}
+
